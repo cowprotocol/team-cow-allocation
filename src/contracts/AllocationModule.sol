73a3d621b4ab104b49fe1ae230ff2eb15a328fc1
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-04-29 10:37:09 +0200

    CowTokens -> CowProtocolTokens

--- src/contracts/AllocationModule.sol
+++ src/contracts/AllocationModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8;
 
-import "./interface/CowTokens.sol";
+import "./interface/CowProtocolTokens.sol";
 import "./vendored/Enum.sol";
 import "./vendored/ModuleController.sol";
 

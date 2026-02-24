2d6ba261e82388e378044b6e664d8bf9ce49521f
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2026-02-24 11:57:30 +0000

    Fix missing variable deployment name

--- src/deploy/001_AllocationModule.ts
+++ src/deploy/001_AllocationModule.ts
@@ -6,14 +6,19 @@ import {
   buildEnableModuleTx,
   constructorInput,
   CONTRACT_NAME,
-  DAO_ALLOCATION_DEPLOYMENT_NAME,
+  TEAM_ALLOCATION_DEPLOYMENT_NAME,
   TEAM_CONTROLLER_SAFE,
   VIRTUAL_COW_TOKEN,
 } from "../ts";
 
-export function allocationModuleDeployFunction(
-  controller: string,
-): DeployFunction {
+export interface AllocationModuleDeploymentInfo {
+  controller: string;
+  deploymentName: string;
+}
+export function allocationModuleDeployFunction({
+  controller,
+  deploymentName,
+}: AllocationModuleDeploymentInfo): DeployFunction {
   return async ({
     deployments,
     getNamedAccounts,
@@ -22,7 +27,7 @@ export function allocationModuleDeployFunction(
     const { deployer } = await getNamedAccounts();
     const { deploy, log } = deployments;
 
-    const { address, abi } = await deploy(DAO_ALLOCATION_DEPLOYMENT_NAME, {
+    const { address, abi } = await deploy(deploymentName, {
       contract: CONTRACT_NAME,
       from: deployer,
       gasLimit: 2000000,
@@ -45,7 +50,9 @@ export function allocationModuleDeployFunction(
   };
 }
 
-const deployAuthenticator: DeployFunction =
-  allocationModuleDeployFunction(TEAM_CONTROLLER_SAFE);
+const deployAuthenticator: DeployFunction = allocationModuleDeployFunction({
+  controller: TEAM_CONTROLLER_SAFE,
+  deploymentName: TEAM_ALLOCATION_DEPLOYMENT_NAME,
+});
 
 export default deployAuthenticator;
